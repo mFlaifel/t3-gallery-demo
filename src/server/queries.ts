2c631669251f78d89256d1991d75c20fcbc65889
mFlaifel <msf-2oo7@hotmail.com>
2024-05-12 23:03:01 +0300

    break queries to seprated file on server

---
+++ src/server/queries.ts
@@ -0,0 +1,21 @@
+import "server-only";
+import { db } from "./db";
+import { auth } from "@clerk/nextjs/server";
+
+export async function getImages() {
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return images;
+}
+
+export async function getMyImages() {
+  const user = auth();
+  if (!user.userId) throw new Error("Not authorized");
+
+  const images = await db.query.images.findMany({
+    where: (model, { eq }) => eq(model.userId, user.userId),
+    orderBy: (model, { desc }) => desc(model.id),
+  });
+  return images;
+}
